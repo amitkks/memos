a483fe4932caaf2cb44e704e280175b06cebedd9
Amit Kumar Sinha <165559490+amitkks@users.noreply.github.com>
2025-08-17 17:29:44 +0530

    Fixed env variables for frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 
-const BASE_URL = import.meta.env.MODE === "development" ?  "http://localhost:5001/api" : `${process.env.PROD_URL}/api`
+const mode = import.meta.env.VITE_NODE_ENV
+console.log(mode)
+
+const BASE_URL = mode === "development" ?  "http://localhost:5001/api" : `${import.meta.env.VITE_PROD_URL}`
 
 const api = axios.create({
     baseURL: BASE_URL, 
